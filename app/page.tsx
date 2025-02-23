485f98a6e2390e37ce034441ec41685d8f848138
toyozato <toyozato@ashibit.net>
2025-02-24 04:08:11 +0900

    プロキシを追加,テスト用のコード追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
+"use client";
 import { Link } from "@heroui/link";
 import { Snippet } from "@heroui/snippet";
 import { Code } from "@heroui/code";
 import { button as buttonStyles } from "@heroui/theme";
+import { useEffect } from "react";
 
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 
 export default function Home() {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => console.log(res.text()));
+  }, []);
+
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
